b05342756ca818a385d2db33f5a7125c0ad64a8e
Виктория Хоружая <khoruzhaia@sfedu.ru>
2021-10-21 09:04:58 +0300

    Revert "Add SecondsInMinute function"
    This reverts commit 4b729680742f5bb7bdd861806da9ed38e2413abe.

--- DateTime.pas
+++ DateTime.pas
@@ -1,14 +1,10 @@
-﻿/// проверить год на високосность
-function IsLeapYear(year: integer): boolean;
+﻿function IsLeapYear(year: integer): boolean;
 begin
   if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then
     Result := True
   else
     Result := False;
 end;
-
-/// количество секунд в минуте
-function SecondsInMinute(m: integer) := m*60;
 
 begin
   
