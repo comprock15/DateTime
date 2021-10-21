4b729680742f5bb7bdd861806da9ed38e2413abe
Виктория Хоружая <khoruzhaia@sfedu.ru>
2021-10-21 09:04:24 +0300

    Add SecondsInMinute function

--- DateTime.pas
+++ DateTime.pas
@@ -1,10 +1,14 @@
-﻿function IsLeapYear(year: integer): boolean;
+﻿/// проверить год на високосность
+function IsLeapYear(year: integer): boolean;
 begin
   if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then
     Result := True
   else
     Result := False;
 end;
+
+/// количество секунд в минуте
+function SecondsInMinute(m: integer) := m*60;
 
 begin
   
