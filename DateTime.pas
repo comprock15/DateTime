f5dcd3e5bdce806cb76a199dc73e72941a3aa266
Виктория Хоружая <khoruzhaia@sfedu.ru>
2021-10-21 09:12:52 +0300

    Add LaterInYear function

--- DateTime.pas
+++ DateTime.pas
@@ -1,9 +1,19 @@
-﻿function IsLeapYear(year: integer): boolean;
+﻿/// проверка года на високосность
+function IsLeapYear(year: integer): boolean;
 begin
   if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then
     Result := True
   else
     Result := False;
+end;
+
+/// проверка какой из двух дней ближе к Новому году
+function LaterInYear(d1, d2: integer): integer;
+begin
+  if d1 > d2 then 
+    Result := 1
+  else
+    Result := 2;
 end;
 
 begin
